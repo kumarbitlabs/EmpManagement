20cd006a24c4cdbd35fbe7f3a58cac122e22d7ce
kumarbitlabs <108260961+kumarbitlabs@users.noreply.github.com>
2023-06-18 00:25:43 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/model/Emp.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Emp {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private String address;
+
+	private Double salary;
+
+	public void setId(int id2) {
+		this.id=id2;
+		
+	}
+}
